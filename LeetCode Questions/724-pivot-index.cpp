d6759374561cd7c13b912ed84d84dca183b45dcf
Desh Iyer <desh.iyer@gmail.com>
2022-10-22 19:53:18 +0530

    Deleted unnecessary .exe files

---
+++ LeetCode Questions/724-pivot-index.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int> arr) {
+    int index;
+
+    
+
+    return index;
+}
+
+int main() {
+    int t = 1;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
