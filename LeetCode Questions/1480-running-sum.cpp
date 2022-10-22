38c7ab8b8d76c851763f5b894da988e5ba46fb12
Desh Iyer <desh.iyer@gmail.com>
2022-10-22 19:09:58 +0530

    Added solution to 1480

---
+++ LeetCode Questions/1480-running-sum.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(vector<int> in) {
+    vector<int> ret = in;
+}
+
+int main() {
+    int t = 1;
+
+    vector<int> in = {1, 2, 3, 4};
+
+    while (t--) {
+        solve(in);
+    }
+
+    return 0;
+}
