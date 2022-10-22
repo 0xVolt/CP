802b2a3ffc5f33fd26511fe7af9813a185367b49
Desh Iyer <desh.iyer@gmail.com>
2022-10-22 21:40:17 +0530

    Scrapped solution to 724

--- LeetCode Questions/724-pivot-index.cpp
+++ LeetCode Questions/724-pivot-index.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve(vector<int> arr) {
+int solve(vector<int> &arr) {
     int index = -1;
 
     for (int i = 0; i < arr.size(); ++i) {
@@ -25,41 +25,14 @@ int solve(vector<int> arr) {
     return index;
 }
 
-int solve_better(vector<int> arr) {
-    int index = -1;
-
-    int sum_right = 0, sum_left = arr[0];
-    vector<int>::iterator initial_iter = arr.begin() + 1;
-
-    for (initial_iter; initial_iter != arr.end(); ++initial_iter) {
-        sum_right += *initial_iter;
-    }
-
-    cout << "\nInitially: " << sum_left << " | " << sum_right;
-
-    vector<int>::iterator it = arr.begin() + 1;
-
-    // while (sum_left != sum_right && it <= arr.end()) {
-    //     it++;
-
-
-
-    //     sum_left += *it;
-    //     sum_right -= *it;
-
-    //     cout << "\n" << sum_left << " | " << sum_right << " | " << *it;
-
-    //     if (sum_left == sum_right)
-    //         index = distance(it, arr.begin());
-    // }
-
-    return index;
+int solve_better(vector<int> &arr) {
+    
 }
 
 int main() {
     int t = 1;
 
-    vector<int> in = {1,7,3,6,5,6};
+    vector<int> in = {1, 7, 3, 6, 5, 6};
 
     while (t--) {
         int i = solve_better(in);
