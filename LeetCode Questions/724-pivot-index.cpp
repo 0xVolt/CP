767c5c43798d7a3af6e3fa53193152db5494ce87
Desh Iyer <desh.iyer@gmail.com>
2022-10-22 20:16:24 +0530

    Added solution to 724

--- LeetCode Questions/724-pivot-index.cpp
+++ LeetCode Questions/724-pivot-index.cpp
@@ -2,18 +2,42 @@
 using namespace std;
 
 int solve(vector<int> arr) {
-    int index;
+    int index = -1;
 
-    
+    for (int i = 0; i < arr.size(); ++i) {
+        int sum_left = 0, sum_right = 0;
+
+        for (int j = 0; j <= i; ++j) {
+            sum_left += arr.at(j);
+        }
+
+        for (int j = i; j < arr.size(); ++j) {
+            sum_right += arr.at(j);
+        }
+
+        if (sum_left == sum_right) {
+            index = i;
+
+            break;
+        }
+    }
 
     return index;
+}
+
+int solve_better(vector<int> arr) {
+    
 }
 
 int main() {
     int t = 1;
 
+    vector<int> in = {1, 7, 3, 6, 5, 6};
+
     while (t--) {
-        solve();
+        int i = solve(in);
+        
+        cout << "Pivot Index: " << i;
     }
 
     return 0;
