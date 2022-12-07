bc6dfc1dfc58e82200cf1df9d946040fe9cef998
Desh Iyer <desh.iyer@gmail.com>
2022-12-07 22:46:43 +0530

    Updated 2.cpp

--- BMC Coding Round/2-alt.cpp
+++ BMC Coding Round/2-alt.cpp
@@ -1,28 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     vector<int> array;
 
     // Inputting till a -ve
     int input;
-    do {
+    do
+    {
         cin >> input;
 
         if (input > 0)
             array.push_back(input);
-    } while(input >= 0);
+    } while (input >= 0);
 
-    for (auto i = array.begin(); i != array.end(); i++) {
+    for (auto i = array.begin(); i != array.end(); i++)
+    {
         // Thanks KT!
-        if (*i < 100 && *(i + 1) < 100) {
+        if (*i < 100 && *(i + 1) < 100)
+        {
             // Alternative solution using for loop
-            for (auto j = i + 1; j != array.end(); j++) {
-                if (*j < 100) {
+            for (auto j = i + 1; j != array.end(); j++)
+            {
+                if (*j < 100)
+                {
                     *i += *j;
                     array.erase(j);
                     j--;
-                } else {
+                }
+                else
+                {
                     break;
                 }
             }
@@ -31,7 +39,8 @@ int main() {
         }
     }
 
-    for (auto i = array.begin(); i != array.end(); i++) {
+    for (auto i = array.begin(); i != array.end(); i++)
+    {
         cout << *i << endl;
     }
 
--- BMC Coding Round/2.cpp
+++ BMC Coding Round/2.cpp
@@ -1,24 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     vector<int> array;
 
     // Inputting till a -ve
     int input;
-    do {
+    do
+    {
         cin >> input;
 
         if (input > 0)
             array.push_back(input);
-    } while(input >= 0);
+    } while (input >= 0);
 
-    for (auto i = array.begin(); i != array.end(); i++) {
+    for (auto i = array.begin(); i != array.end(); i++)
+    {
         // Thanks KT!
-        if (*i < 100 && *(i + 1) < 100) {
+        if (*i < 100 && *(i + 1) < 100)
+        {
             auto j = i;
 
-            while(*(++j) < 100) {
+            while (*(++j) < 100)
+            {
                 *i += *j;
                 array.erase(j--);
             }
@@ -27,7 +32,8 @@ int main() {
         }
     }
 
-    for (auto i = array.begin(); i != array.end(); i++) {
+    for (auto i = array.begin(); i != array.end(); i++)
+    {
         cout << *i << endl;
     }
 
