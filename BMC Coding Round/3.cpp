00c513e8da677c99a6646c19d09836e5eaa028d0
Desh Iyer <desh.iyer@gmail.com>
2022-12-07 22:47:12 +0530

    Updated 3.cpp

--- BMC Coding Round/3.cpp
+++ BMC Coding Round/3.cpp
@@ -1,20 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     vector<int> array;
 
     // Inputting till a -ve
     int input;
-    do {
+    do
+    {
         cin >> input;
 
         if (input > 0)
             array.push_back(input);
-    } while(input >= 0);
+    } while (input >= 0);
 
     int highest = 0;
-    for (int i = 0; i < array.size(); i++) {
+    for (int i = 0; i < array.size(); i++)
+    {
         if (array[i] > highest && array[i] < 100)
             highest = array[i];
     }
