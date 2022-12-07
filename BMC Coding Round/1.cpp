421e3869848eb8f8d0ceb8d30b045c728014441b
Desh Iyer <desh.iyer@gmail.com>
2022-12-07 22:46:20 +0530

    Updated 1.cpp

--- BMC Coding Round/1.cpp
+++ BMC Coding Round/1.cpp
@@ -1,17 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     vector<int> array;
 
     // Inputting till a -ve
     int input;
-    do {
+    do
+    {
         cin >> input;
 
         if (input > 0)
             array.push_back(input);
-    } while(input >= 0);
+    } while (input >= 0);
 
     // Test code
     // for (int i = 0; i < array.size(); i++) {
@@ -19,7 +21,8 @@ int main() {
     // }
 
     int count = 0;
-    for (int i = 0; i < array.size(); i++) {
+    for (int i = 0; i < array.size(); i++)
+    {
         if (array[i] % 6 == 0)
             count++;
     }
