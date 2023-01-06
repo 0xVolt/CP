fbf129629d7556f147671453769336629416816b
Desh Iyer <desh.iyer@gmail.com>
2023-01-06 19:47:28 +0400

    Added solution to INSTAGRAM

---
+++ CodeChef/INSTAGRAM.cpp
@@ -0,0 +1,25 @@
+// Problem Code: INSTAGRAM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        int x, y;
+
+        cin >> x >> y;
+
+        if (x > (10 * y)) {
+            cout << "YES";
+        } else 
+            cout << "NO";
+
+        cout << "\n";
+    }
+
+    return 0;
+}
