a041e0e5decc97bbe43e489229e95f65885f54bd
Desh Iyer <desh.iyer@gmail.com>
2023-01-06 19:34:02 +0400

    Added solution to CABS

---
+++ CodeChef/CABS.cpp
@@ -0,0 +1,27 @@
+// Problem Code: CABS 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        int X, Y;
+
+        cin >> X >> Y;
+        
+        if (X > Y) {
+            cout << "FIRST";
+        } else if (X < Y)  
+            cout << "SECOND";
+        else
+            cout << "ANY";
+            
+        cout << "\n";
+    }
+
+    return 0;
+}
