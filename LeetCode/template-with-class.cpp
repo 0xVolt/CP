50cb57a0738f4f8e758b887365f153b482af43be
Desh Iyer <desh.iyer@gmail.com>
2023-03-12 20:25:10 +0530

    Add new template
    Template now includes submissions as methods to a Solution class

---
+++ LeetCode/template-with-class.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int solve() {
+            
+        }
+};
+
+int main() {
+    Solution S;
+
+    S.solve();
+}
