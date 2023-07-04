ea71ad4275997a7c59d77dbb512fcf98918b0fda
Desh Iyer <desh.iyer@gmail.com>
2023-07-04 22:25:45 +0400

    Update return vector

--- LeetCode/1-two-sum.cpp
+++ LeetCode/1-two-sum.cpp
@@ -8,7 +8,7 @@ class twosum
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        vector<int> retrn;
+        vector<int> returnNumber;
         int diff;
         int size = nums.size();
         unordered_map<int, int> m;
@@ -17,12 +17,12 @@ public:
             diff = target - nums[i];
             if (m.find(diff) != m.end() && m.find(diff)->second != i)
             {
-                retrn.push_back(i);
-                retrn.push_back(m.find(diff)->second);
-                return retrn;
+                returnNumber.push_back(i);
+                returnNumber.push_back(m.find(diff)->second);
+                return returnNumber;
             }
             m[nums[i]] = i;
         }
-        return retrn;
+        return returnNumber;
     }
 };
