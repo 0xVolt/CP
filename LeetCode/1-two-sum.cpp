8061a0d16a3375f3d149c72b14eaff2b77fd981c
Desh Iyer <desh.iyer@gmail.com>
2023-08-09 00:28:56 +0530

    Edit vector names

--- LeetCode/1-two-sum.cpp
+++ LeetCode/1-two-sum.cpp
@@ -3,24 +3,22 @@
 #include <unordered_map>
 using namespace std;
 
-class twosum
-{
+class twosum {
 public:
-    vector<int> twoSum(vector<int> &nums, int target)
-    {
+    vector<int> twoSum(vector<int> &nums, int target) {
         vector<int> returnNumber;
         int diff;
         int size = nums.size();
         unordered_map<int, int> m;
-        for (int i = 0; i < size; i++)
-        {
+        
+        for (int i = 0; i < size; i++) {
             diff = target - nums[i];
-            if (m.find(diff) != m.end() && m.find(diff)->second != i)
-            {
+            if (m.find(diff) != m.end() && m.find(diff)->second != i) {
                 returnNumber.push_back(i);
                 returnNumber.push_back(m.find(diff)->second);
                 return returnNumber;
             }
+            
             m[nums[i]] = i;
         }
         return returnNumber;
